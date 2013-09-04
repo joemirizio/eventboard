257609439d70a85ca3af87525e921ec737c5e332
Joe Mirizio <joe@joemirizio.com>
2013-09-03 23:27:40 -0400

    Change name and gemset to 'eventboard'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-EventBoard::Application.config.session_store :cookie_store, key: '_event_board_session'
+EventBoard::Application.config.session_store :cookie_store, key: '_eventboard_session'
