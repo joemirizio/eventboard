9304e29a817134861ef960884e26889cdb89457b
Joe Mirizio <joe@joemirizio.com>
2013-09-09 00:36:53 -0400

    Fix chrome input rendering issue. Add Poll update action

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -15,6 +15,16 @@ class PollsController < ApplicationController
 		@poll = Poll.find(params[:id])
 	end
 
+	def update
+		@poll = Poll.find(params[:id])
+
+		if @poll.update(poll_params)
+			redirect_to @poll
+		else
+			render 'edit'
+		end
+	end
+
 	def destroy
 		@event = Event.find(params[:event_id])
 		@poll = @event.polls.find(params[:id])
